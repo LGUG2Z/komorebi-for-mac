f93cf44aba7ff41d7e1b76a212d8371cb208b747
LGUG2Z <LGUG2Z@fastmail.com>
2025-10-26 12:05:46 -0700

    ci(github): attach homebrew formula to nightly release

---
+++ formula.template.rb
@@ -0,0 +1,30 @@
+class KomorebiForMacNightly < Formula
+  desc "Tiling window manager for macOS (nightly build)"
+  homepage "https://github.com/KomoCorp/komorebi-for-mac"
+  url "https://api.github.com/repos/KomoCorp/komorebi-for-mac/releases/assets/REPLACE_ASSET_ID",
+      headers: [
+        "Accept: application/octet-stream",
+        "X-GitHub-Api-Version: 2022-11-28",
+        "Authorization: bearer #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}",
+      ]
+  version "nightly"
+  sha256 "REPLACE_SHA_256"
+  license "Komorebi License 2.0.0"
+
+  def install
+    bin.install Dir["*"]
+  end
+
+  def caveats
+    <<~EOS
+      This formula requires access to a private GitHub repository.
+      Make sure you have set your GitHub token:
+
+      export HOMEBREW_GITHUB_API_TOKEN=your_token_here
+    EOS
+  end
+
+  test do
+    system "#{bin}/komorebi", "--help"
+  end
+end
